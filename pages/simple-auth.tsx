5ec39cebfadac5785cc9101c594aadbde76cd5f1
lev <lev@kermy.org>
2026-09-19 14:09:34 -0300

    feat: enhance authentication flow with refresh token handling and loading states

---
+++ pages/simple-auth.tsx
@@ -0,0 +1,5 @@
+import ClientApp from "../src/ClientAppOnly";
+
+export default function SimpleAuthRoute() {
+  return <ClientApp />;
+}
---
+++ src/utils/showSimpleAuth.js
@@ -0,0 +1,3 @@
+const showSimpleAuth = process.env.REACT_APP_SHOW_SIMPLE_AUTH === 'true';
+
+export default showSimpleAuth;
